231267794082b16f6672821b4a40fdb6eea8984e
Kai Xu <xukai921110@gmail.com>
2020-03-23 23:58:19 +0000

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,9 @@
-import Conda
+import Conda, Pkg
 Conda.add("pip")
 
 PIP_PATH = joinpath(Conda.ROOTENV, "bin/pip")
 run(`$PIP_PATH install wandb`)
+
+PYTHON_PATH = joinpath(Conda.ROOTENV, "bin/python")
+ENV["PYTHON"] = PYTHON_PATH
+Pkg.build("PyCall")
