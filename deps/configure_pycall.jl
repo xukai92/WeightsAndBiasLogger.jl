b5ee42cc085460270c6325e77cad725d472748e3
Kai Xu <xukai921110@gmail.com>
2020-03-24 16:27:36 +0000

    fix build

--- deps/configure_pycall.jl
+++ deps/configure_pycall.jl
@@ -6,6 +6,5 @@ if !isfile(pip)
     println("`pip` is not available in the current PyCall.jl")
     println("Configuring PyCall.jl to use Conda.jl")
     ENV["PYTHON"] = ""
-    rm(Pkg.dir("PyCall", "deps", "PYTHON"))
     Pkg.build("PyCall")
 end
