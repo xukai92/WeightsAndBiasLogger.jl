d8fab3987bd19d535d2e34fb78c16daff2d95be4
Kai Xu <xukai921110@gmail.com>
2020-03-24 15:56:14 +0000

    fix PyCall rebuild

--- deps/build.jl
+++ deps/build.jl
@@ -8,6 +8,7 @@ catch
     println("`pip` is not available in the current PyCall.jl")
     println("Configuring PyCall.jl to use Conda.jl")
     ENV["PYTHON"] = joinpath(Conda.PYTHONDIR, "bin/python")
+    rm(Pkg.dir("PyCall", "deps", "PYTHON"))
     Pkg.build("PyCall")
 end
 
