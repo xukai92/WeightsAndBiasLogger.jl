2ae411577c13c06e2e8f24dfc02705e0dc185449
Kai Xu <xukai921110@gmail.com>
2020-03-24 15:22:59 +0000

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,16 @@
-import Conda
-ENVPATH = Conda.ROOTENV
-Conda.add("pip")
-PIP = joinpath(ENVPATH, "bin/pip")
-run(`$PIP install wandb matplotlib`)
+using PyCall
 
-import Pkg
-PYTHON = joinpath(ENVPATH, "bin/python")
-ENV["PYTHON"] = PYTHON
-Pkg.build("PyCall")
+PIP = joinpath(split(PyCall.PYTHONHOME, ":")[end], "bin/pip")
+
+function pipinstall(pkg)
+    try
+        pyimport(pkg)
+    catch
+        println("`$pkg` is not available in the current PyCall.")
+        println("Installing using Conda.jl ...")
+        run(`$PIP install $pkg`)
+    end
+end
+
+pipinstall("matplotlib")
+pipinstall("wandb")
