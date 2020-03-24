5af8a0d0bd5f7625039ce72abd81c19aca057b23
Kai Xu <xukai921110@gmail.com>
2020-03-24 01:32:26 +0000

    add demo to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,3 +9,21 @@ using Test
     test2 = Dict(:x => 1, :y => "x")
     @test string_dict("", test2; ignores=[:y]) == Dict("x" => 1)
 end
+
+@testset "Demo" begin
+    using Logging, WeightsAndBiasLogger
+
+    logger = WBLogger(project="sample-project")
+
+    args = (n_epochs=1_000, lr=1e-3)
+
+    config!(logger, args)
+
+    with_logger(logger) do
+        loss = 0
+        for i in 1:args.n_epochs
+            loss += randn()
+            @info "train" i=i loss=loss
+        end
+    end
+end
