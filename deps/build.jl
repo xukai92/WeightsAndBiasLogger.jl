b0fafc3cf0dc97858d876572070e6e51bed826da
Kai Xu <xukai921110@gmail.com>
2020-03-24 00:23:35 +0000

    fix order

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,10 @@
-import Pkg
-PYTHON_PATH = joinpath(Conda.ROOTENV, "bin/python")
-ENV["PYTHON"] = PYTHON_PATH
-Pkg.build("PyCall")
-
 import Conda
 Conda.add("pip")
-PIP_PATH = joinpath(Conda.ROOTENV, "bin/pip")
-run(`$PIP_PATH install wandb matplotlib`)
+ENVPATH = Conda.ROOTENV
+PIP = joinpath(ENVPATH, "bin/pip")
+run(`$PIP install wandb matplotlib`)
+
+import Pkg
+PYTHON = joinpath(ENVPATH, "bin/python")
+ENV["PYTHON"] = PYTHON
+Pkg.build("PyCall")
